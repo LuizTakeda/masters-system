b4a010eac19065e62c9832391e3392f9138b6cd0
Luiz Takeda <luizgustavoglt@gmail.com>
2026-08-25 10:51:59 +0100

    feat(web:fiware): add context file hooks and service

---
+++ apps/web/src/pages/dashboard/project/context/page.tsx
@@ -0,0 +1,6 @@
+export default function ContextFilePage(){
+  return (
+    <main>
+    </main>
+  );
+}
--- apps/web/src/pages/dashboard/project/router.tsx
+++ apps/web/src/pages/dashboard/project/router.tsx
@@ -1,9 +1,14 @@
 import type { RouteObject } from "react-router";
 import ProjectHomePage from "./page";
+import ContextFilePage from "./context/page";
 
 export const ProjectDashboardRouter: RouteObject[] = [
   {
     index: true,
     element: <ProjectHomePage />,
   },
+  {
+    path: "context-file",
+    element: <ContextFilePage />
+  }
 ];
